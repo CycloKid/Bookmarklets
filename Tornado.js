04581c7845b2dc3d52e191c6c43864a93d9f0389
CycloKid <63879422+CycloKid@users.noreply.github.com>
2022-01-12 08:46:08 -0500

    Create Tornado.js

---
+++ Tornado.js
@@ -0,0 +1,1 @@
+javascript: (function() {    document.documentElement.style.transitionDuration = "60s";    document.documentElement.style.transitionTimingFunction = "ease-in";    document.documentElement.style.transform = "rotate(360000deg)";}());
